eb6a54917f3231c0e4456e92a02640a15a1b0295
Zomer <zomerzxc.19@gmail.com>
2021-08-30 15:50:31 +0800

    Setup project

--- App.js
+++ App.js
@@ -1,45 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const [name, setName] = useState('Zomer');
-  const [age, setAge] = useState(23);
   return (
-    <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        placeholder='e.g. John Doe'
-        onChangeText={val => setName(val)}
-      />
-      <Text>Enter age:</Text>
-      <TextInput
-        keyboardType='numeric'
-        style={styles.input}
-        placeholder='e.g. 20'
-        onChangeText={val => setAge(val)}
-      />
-      <Text>
-        Name: {name}, age: {age}
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Fire Todo' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  },
-});
