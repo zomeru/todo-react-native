06fbb3a4655b1bd873799ee37856b06d2bca0ade
Zomer <zomerzxc.19@gmail.com>
2021-08-31 16:59:36 +0800

    Add edit mode and delete button

--- screens/TodoList.js
+++ screens/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,13 +6,69 @@ import {
   TouchableOpacity,
   FlatList,
 } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import TodoItem from '../components/TodoItem';
 import Colors from '../constants/Colors';
 
-export default () => {
+const renderAddListIcon = addItem => {
+  return (
+    <TouchableOpacity onPress={() => addItem({ text: '', isChecked: false })}>
+      <Text style={styles.icon}>+</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ({ navigation }) => {
+  const [todoItems, setTodoItems] = useState([
+    { text: 'Hello', isChecked: false },
+  ]);
+
+  const addItemToLists = item => {
+    todoItems.push(item);
+    setTodoItems([...todoItems]);
+  };
+
+  const removeItemFromLists = index => {
+    todoItems.splice(index, 1);
+    setTodoItems([...todoItems]);
+  };
+
+  const updateItem = (index, item) => {
+    todoItems[index] = item;
+    setTodoItems([...todoItems]);
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => renderAddListIcon(addItemToLists),
+    });
+  });
+
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <FlatList
+        data={todoItems}
+        renderItem={({ item: { text, isChecked }, index }) => {
+          return (
+            <TodoItem
+              text={text}
+              isChecked={isChecked}
+              onChecked={() => {
+                const todoItem = todoItems[index];
+                todoItem.isChecked = !isChecked;
+                updateItem(index, todoItem);
+              }}
+              onChangeText={newText => {
+                const todoItem = todoItems[index];
+                todoItem.text = newText;
+                updateItem(index, todoItem);
+              }}
+              onDelete={() => {
+                removeItemFromLists(index);
+              }}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
